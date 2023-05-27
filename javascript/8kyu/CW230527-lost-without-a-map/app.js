14fcc1be326beb1e555e5d95e17e183771fdaff2
Greg Johnston <gregmj.sco@gmail.com>
2023-05-27 14:00:03 +0900

    Add my solution to JS 8kyu lost without a map

---
+++ javascript/8kyu/CW230527-lost-without-a-map/app.js
@@ -0,0 +1,16 @@
+/*Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+
+function maps(x){
+
+    let array = [];
+    for(let i = 0; i < x.length; i++){
+    array.push(x[i] * 2);
+    }
+    return array;
+    }

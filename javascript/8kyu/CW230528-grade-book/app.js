c3632a5badde2852eb390cb6e03afad7536c8ff6
Greg Johnston <gregmj.sco@gmail.com>
2023-05-28 13:27:40 +0900

    Add solution for grade book

--- javascript/8kyu/CW230528-grade-book/app.js
+++ javascript/8kyu/CW230528-grade-book/app.js
@@ -11,3 +11,19 @@ Numerical Score 	Letter Grade
 Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 */
 
+function getGrade (s1, s2, s3) {
+    // Code here
+    average = (s1 + s2 + s3) / 3;
+    
+    if (average < 60){
+      return "F";
+    } else if (average >= 60 && average < 70) {
+      return "D";
+    } else if (average >= 70 && average < 80) {
+      return "C";
+  } else if (average >= 80 && average < 90) {
+      return "B";
+    } else if (average >= 90 && average <= 100) {
+      return "A";
+      }
+  }

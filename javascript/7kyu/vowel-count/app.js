3aad2c05d07cef69f8ac9ffd16c0a6beba453e82
Greg Johnston <gregmj.sco@gmail.com>
2023-07-23 12:23:18 +0900

    Add finished solution for vowel count

--- javascript/7kyu/vowel-count/app.js
+++ javascript/7kyu/vowel-count/app.js
@@ -1,14 +1,13 @@
 function getCount(str) {
-    array = str.split("");
-    console.log(array)
-    count = 0;
-    console.log(count)
-    const myFunc = letter => {
-      if (letter == 'a' || 'i' || 'u' || 'e' || 'o') 
-        count++
-      console.log()
+  array = str.split("");
+  console.log(array);
+  count = 0;
+  console.log(count);
+  for (let x in array){
+    console.log(array[x]);
+    if(array[x] == 'a' || array[x] == 'u' || array[x] == 'i' || array[x] == 'e' || array[x] == 'o') {
+      count++;
     }
-    array.forEach(myFunc(item))
-    
-    return count;
   }
+  return count;
+}

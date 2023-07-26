0275ea1e3fa0237c546fb72c0839d5a22d44cc4c
Greg Johnston <gregmj.sco@gmail.com>
2023-07-26 15:47:52 +0900

    Add working solution

--- javascript/7kyu/grownth-population/app.js
+++ javascript/7kyu/grownth-population/app.js
@@ -1,17 +1,20 @@
 function nbYear(p0, percent, aug, p) {
     // your code
+  console.log({p});
   let per = percent / 100;
-  console.log(per)
+  console.log(per);
   let yearCount = 0;
-  let newPop = 0;
+  let newPop = p0;
+  console.log({newPop});
   
   while(newPop < p){
-    newPop += p0 + (p0 * per) + aug;
+    console.log({newPop}, {p0}, {per}, {aug});
+    newPop = Math.floor(newPop + (newPop * per) + aug)
     console.log(newPop);
     yearCount++;
-    console.log(yearCount)
-    if (newPop > p)
-      return yearCount
+    console.log({yearCount});
+    
+    if (newPop >= p)
+      return yearCount;
   }
-  
 }
